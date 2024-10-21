d77d898ffd925a4bbf3e64adf06c5352a2438352
mel2134 <158561962+mel2134@users.noreply.github.com>
2024-10-21 16:31:14 +0300

    Added ImageViewModel

---
+++ TrainHoppersTARpe23/Models/Abilities/AbilityImageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TrainHoppersTARpe23.Models.Abilities
+{
+    public class AbilityImageViewModel
+    {
+        public Guid ImageID { get; set; }
+        public string ImageTitle { get; set; }
+        public byte[] ImageData { get; set; }
+        public string Image { get; set; }
+        public Guid? AbilityID { get; set; }
+    }
+}
