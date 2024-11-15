0d908940699ab5c280f6e77c32e70ddab8a053bc
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2024-11-15 11:40:22 +0200

    Powerup class

---
+++ TrainHoppers.Core/Domain/Powerup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainHoppers.Core.Domain
+{
+    public enum PowerupType
+    {
+        Heal, PlotArmor, Ressurect, WhiteFlag, CarrotMayhem
+    }
+    public class Powerup
+    {
+        public string PowerUpName { get; set; }
+        public string PowerUpDescription { get; set; }
+        public int TotalUses { get; set; }
+        public int UsedTime { get; set; }
+        public PowerupType PowerupType { get; set; }
+
+    }
+}
