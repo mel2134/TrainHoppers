bce6dc839e9c468956e9db0b3675913dc3769600
DorianTvk <158561962+mel2134@users.noreply.github.com>
2024-11-20 15:20:38 +0200

    Added AccountsServices and its interface

---
+++ TrainHoppers.ApplicationServices/Services/AccountsServices.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainHoppers.Core.Domain;
+
+namespace TrainHoppers.ApplicationServices.Services
+{
+    public class AccountsServices : IAccountsServices
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public AccountsServices(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+    }
+}
