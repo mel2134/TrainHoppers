4f78ef03c45cc43cfa43e77cdb20b1544498df14
DorianTvk <158561962+mel2134@users.noreply.github.com>
2024-11-20 15:05:02 +0200

    Added methods to Addpassword and added view too

---
+++ TrainHoppersTARpe23/Controllers/AccountsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TrainHoppers.Core.Domain;
+using TrainHoppers.Data;
+using TrainHoppersTARpe23.Models.Accounts;
+
+namespace TrainHoppersTARpe23.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly TrainHoppersContext _context;
+        public AccountsController(UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager, TrainHoppersContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddPassword()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var userHasPassword = await _userManager.HasPasswordAsync(user);
+            if (userHasPassword)
+            {
+                RedirectToAction("ChangePassword");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPassword(AddPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var result = await _userManager.AddPasswordAsync(user, model.NewPassword);
+                if (!result.Succeeded)
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View();
+                }
+                await _signInManager.RefreshSignInAsync(user);
+                return View("AddPasswordConfirmation");
+            }
+            return View(model);
+        }
+        
+    }
+}
