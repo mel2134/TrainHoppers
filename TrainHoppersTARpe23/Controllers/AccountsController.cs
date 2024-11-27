2a4275d0c6800a29cf6c198fcd9b3b5a5bd14efc
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2024-11-27 14:00:04 +0200

    Added change password view, mehod and 2 missing models .

--- TrainHoppersTARpe23/Controllers/AccountsController.cs
+++ TrainHoppersTARpe23/Controllers/AccountsController.cs
@@ -49,6 +49,34 @@ namespace TrainHoppersTARpe23.Controllers
             }
             return View(model);
         }
-        
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if (user != null)
+                {
+                    return RedirectToAction("Login");
+                }
+                var result = await _userManager.ChangePasswordAsync(user,model.CurrentPassword,model.NewPassword);
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View();
+                }
+                await _signInManager.RefreshSignInAsync(user);
+                return View("ChangePasswordConfirmation");
+            }
+            return View(model);
+        }
     }
 }
