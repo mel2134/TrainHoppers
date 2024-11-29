3e00194d2b7280a3d7fcf06282270ed9e5154083
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2024-11-29 10:27:26 +0200

    Added methods for Register,Confirm,Login,Logout

--- TrainHoppersTARpe23/Controllers/AccountsController.cs
+++ TrainHoppersTARpe23/Controllers/AccountsController.cs
@@ -103,5 +103,115 @@ namespace TrainHoppersTARpe23.Controllers
             }
             return View(model);
         }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    City = model.City,
+                };
+                var result = await _userManager.CreateAsync(user,model.Password);
+                if (result.Succeeded)
+                {
+                    var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var confirmLink = Url.Action("ConfirmEmail","Accounts", new { userId = user.Id, token = token }, Request.Scheme);
+                    if (_signInManager.IsSignedIn(User) && User.IsInRole("Admin"))
+                    {
+                        return RedirectToAction("ListUsers","Administrations");
+                    }
+                    ViewBag.ErrorTitle = "Successfully registered";
+                    ViewBag.ErrorMessage = "Before you can login, please confirm your email. An email has been sent to you.";
+                    return View("Error");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View();
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConfirmEmail(string userId,string token)
+        {
+            if(userId == null || token == null)
+            {
+                return RedirectToAction("Index","Home");
+            }
+            var user = await _userManager.FindByIdAsync(userId);
+            if(user == null)
+            {
+                ViewBag.ErrorMessage = $"The user with id of {userId} does not exist";
+                return View("NotFound");
+            }
+            var result = await _userManager.ConfirmEmailAsync(user, token);
+            if (result.Succeeded)
+            {
+                return View();
+            }
+            ViewBag.ErrorTitle = "Email cannot be confirmed";
+            ViewBag.ErrorMessage = $"The users emails with userId {userId} cannot be confirmed.";
+            return View("Error");
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(string? returnUrl)
+        {
+            LoginViewModel vm = new()
+            {
+                ReturnUrl = returnUrl,
+            };
+            return View(vm);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model,string? returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if(user != null && !user.EmailConfirmed && (await _userManager.CheckPasswordAsync(user,model.Password)))
+                {
+                    ModelState.AddModelError(string.Empty,"Your email is not confirmed yet. Please confirm it.");
+                    return View(model);
+                }
+                var result = await _signInManager.PasswordSignInAsync(model.Email,model.Password,model.RememberMe,true);
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+
+                    return RedirectToAction("Index","Home");
+                }
+                if (result.IsLockedOut)
+                {
+                    return View("AccountLocked");
+                }
+                ModelState.AddModelError("","Invalid Login Attempt, contact admin");
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index","Home");
+        }
+
     }
 }
