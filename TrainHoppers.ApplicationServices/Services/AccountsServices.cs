c41b327add0a66e407f914366ccf0afe8cad401a
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2024-11-29 11:25:56 +0200

    Added missing loginddto, added methods for logging  in, registering and email confirmation, added login buton to index,

--- TrainHoppers.ApplicationServices/Services/AccountsServices.cs
+++ TrainHoppers.ApplicationServices/Services/AccountsServices.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrainHoppers.Core.Domain;
+using TrainHoppers.Core.Dto.AccountsDtos;
 using TrainHoppers.Core.ServiceInterface;
 
 namespace TrainHoppers.ApplicationServices.Services
@@ -19,6 +20,35 @@ namespace TrainHoppers.ApplicationServices.Services
             _userManager = userManager;
             _signInManager = signInManager;
         }
-
+        public async Task<ApplicationUser> Register(ApplicationUserDto dto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = dto.UserName,
+                Email = dto.Email,
+                City = dto.City,
+            };
+            var result = await _userManager.CreateAsync(user,dto.Password);
+            if (result.Succeeded)
+            {
+                var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            }
+            return user;
+        }
+        public async Task<ApplicationUser> ConfirmEmail(string userId,string token)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                string errorMsg = $"User with id {userId} does not exist";
+            }
+            var result = await _userManager.ConfirmEmailAsync(user, token);
+            return user;
+        }
+        public async Task<ApplicationUser> Login(LoginDto dto)
+        {
+            var user = await _userManager.FindByEmailAsync(dto.Email);
+            return user;
+        }
     }
 }
--- TrainHoppers.Core/ServiceInterface/IAccountsServices.cs
+++ TrainHoppers.Core/ServiceInterface/IAccountsServices.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrainHoppers.Core.Domain;
+using TrainHoppers.Core.Dto.AccountsDtos;
 
 namespace TrainHoppers.Core.ServiceInterface
 {
     public interface IAccountsServices
     {
-
+        Task<ApplicationUser> Register(ApplicationUserDto dto);
+        Task<ApplicationUser> ConfirmEmail(string userId, string token);
+        Task<ApplicationUser> Login(LoginDto dto);
     }
 }
