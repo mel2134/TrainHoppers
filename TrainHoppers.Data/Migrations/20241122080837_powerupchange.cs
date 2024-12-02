9c9d05262d334296ea6bdd5426ae49a278b6109f
Opilane TTHK <>
2024-12-02 19:01:35 +0200

    i start  the project and added view, migrations and db

---
+++ TrainHoppers.Data/Migrations/20241122080837_powerupchange.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainHoppers.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class powerupchange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UsedTime",
+                table: "Powerups",
+                newName: "UsesLeft");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UsesLeft",
+                table: "Powerups",
+                newName: "UsedTime");
+        }
+    }
+}
---
+++ TrainHoppers.Data/Migrations/20241129090719_ajsdkiada1213123.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainHoppers.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ajsdkiada1213123 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
